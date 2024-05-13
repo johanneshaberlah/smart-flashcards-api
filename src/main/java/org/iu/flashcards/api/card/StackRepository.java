ccdde6eaa242e21f1e647e9c45224bdaa2ad84d2
Johannes Haberlah <haberlah.j@zdf.de>
2024-05-13 17:12:42 +0200

    Add REST endpoints

--- src/main/java/org/iu/flashcards/api/card/StackRepository.java
+++ src/main/java/org/iu/flashcards/api/card/StackRepository.java
@@ -1,8 +1,10 @@
 package org.iu.flashcards.api.card;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
+@RestResource
 @Repository
 public interface StackRepository extends JpaRepository<Stack, Long> {
 }
